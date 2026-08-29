06d9e32b366228a23c269bc934706c8e718db8d1
Wasim Ansari <oasimansari0786@gmail.com>
2026-08-29 16:23:07 +0545

    MongoDB connection

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 // Server start 
 
-
+import 'dotenv/config';
 import app from "./src/app.js"
+import { connectDB } from "./src/db/db.js";
 
-app.listen(3000, ()=>{
+connectDB()
+
+app.listen(process.env.PORT, ()=>{
     console.log("Server listen on port:3000");
 })
